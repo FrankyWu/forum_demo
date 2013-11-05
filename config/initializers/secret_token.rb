e71ea2596b5835a5aebb009c37fb6fe964b324f9
FrankyWu <forefeel741114@gmail.com>
2013-11-05 17:37:14 +0800

    p23 type OK

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForumDemo::Application.config.secret_token = 'fe619004bb27c9c5ed6ec149637039b9a5df76d72e0403be21300f3755403d2fb04e431f69b4b65aa2c15fcdf2aa17474a1508f394bd9158e396dcf8485967ef'
